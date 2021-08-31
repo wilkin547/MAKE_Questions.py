40e921e7094dd463d6c2a556ae75eccb65a9fe3f
wilkin trinidad <81598320+wilkin547@users.noreply.github.com>
2021-08-31 05:39:05 -0700

    v01

---
+++ python/enunciado.py
@@ -0,0 +1,11 @@
+class Frase:
+
+    def __init__(self,subject, verb, complement ) -> None:
+
+        self.subject = subject
+        self.verb = verb 
+        self.complement = complement
+        pass
+
+    def sayHelllo(self,mensaje):
+        print (mensaje)
---
+++ python/practica 2.py
@@ -0,0 +1,142 @@
+from typing import ItemsView, NewType
+
+from enunciado import Frase
+import enunciado
+
+
+class miniEnunciado(enunciado):
+    def __init__(self) -> None:
+        super().__init__()
+        
+
+
+
+
+
+
+
+
+palabra = Frase("wilkin","pedro","samuel")
+
+frase = input() + " "
+
+def comprueba_verbs(frase):
+
+
+    verbos = ["is","are","run","go","stay","swim"]
+
+    for verb in verbos:
+
+        if frase == verb + " ":
+            return True
+
+    return False
+def encuentra_Pregunta(phrase):
+
+
+
+    word = ""
+    Question = []
+
+    
+
+    for letter in phrase:
+
+        if  letter != " " :
+
+            word += letter
+        
+        else: 
+
+            Question.append(word + " ")
+            word = ""
+        
+
+    return Question
+
+def Make_Question_open(palabras):
+
+    complento = ""
+    sujeto = ""
+    vervo = ""
+
+    isSubect = True
+    isComplemto = False
+    isVerb = False
+    
+
+    for word in palabras:
+
+        if   comprueba_verbs(word): 
+
+            isSubect = False
+            isVerb = True
+
+        if isSubect :
+
+            sujeto += word
+            
+
+        if isVerb :
+
+            vervo += word
+            isComplemto = True
+            isVerb = False
+            continue
+            
+
+        if isComplemto :
+
+            complento += word
+
+
+    texto_final =  "what " + vervo +  complento + "? " 
+    return texto_final
+
+def estructura_frase(palabras):
+    complento = ""
+    sujeto = ""
+    vervo = ""
+
+    isSubect = True
+    isComplemto = False
+    isVerb = False
+    
+
+    for word in palabras:
+
+
+        if comprueba_verbs(word): 
+
+            isSubect = False
+            isVerb = True
+
+        if isSubect :
+
+            sujeto += word
+            
+
+        if isVerb :
+
+            vervo += word
+            isComplemto = True
+            isVerb = False
+            continue
+            
+
+        if isComplemto :
+
+            complento += word
+
+
+    texto = {
+        "sujeto" : sujeto,
+        "verbo"  : vervo,
+        "complento" : complento
+    }
+    return texto
+
+
+palabra.sayHelllo(frase)
+#print (estructura_frase(encuentra_Pregunta( frase)))
+#encuentra_Pregunta(frase)
